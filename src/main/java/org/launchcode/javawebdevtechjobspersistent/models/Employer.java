59c6f2b86ac24e3bdcd891cad393d43c6bd2e00a
Caroline STone <docphilstone@gmail.com>
2020-07-08 14:37:32 -0500

    Add jobs field to employer, updated Job Model

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,13 +1,20 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
     @NotBlank(message = "Location required")
     private String location;
+
+    @OneToMany
+    @JoinColumn
+    private List<Job> jobs;
 
     public Employer() {
     }
