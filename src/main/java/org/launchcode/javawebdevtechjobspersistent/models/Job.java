b9e553bb4c03dfff7afb7debc4ab033a7bb085b2
Caroline STone <docphilstone@gmail.com>
2020-07-13 18:25:53 -0500

    updated Job model to fit many-to-many relationship with skills

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
@@ -1,6 +1,10 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Job extends AbstractEntity {
@@ -8,15 +12,16 @@ public class Job extends AbstractEntity {
     @ManyToOne
     private Employer employer;
 
-    private String skills;
+    @ManyToMany
+    private final List<Skill> skills = new ArrayList<>();
+
 
     public Job() {
     }
 
-    public Job(Employer anEmployer, String someSkills) {
+    public Job(Employer anEmployer, SkillRepository someSkills) {
         super();
         this.employer = anEmployer;
-        this.skills = someSkills;
     }
 
     // Getters and setters.
@@ -29,11 +34,8 @@ public class Job extends AbstractEntity {
         this.employer = employer;
     }
 
-    public String getSkills() {
+    public List<Skill> getSkills() {
         return skills;
     }
 
-    public void setSkills(String skills) {
-        this.skills = skills;
-    }
 }
